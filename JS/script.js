7458be1c7dff4e1349f693ef20788ba44c7c270b
kimbeomjun <gimb52043@gmial.com>
2022-08-19 17:48:06 +0900

    mcnsports_renewal

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function(){
+    $(".mainWrap").slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        // autoplay:true,
+        cssEase: "linear",
+    });
+});
+
