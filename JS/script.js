afe81352a3f3be0b8bbd76658c960e5f5169d399
kimbeomjun <gimb52043@gmial.com>
2022-08-24 17:42:40 +0900

    mcnsports_renewal

--- JS/script.js
+++ JS/script.js
@@ -3,9 +3,33 @@ $(document).ready(function(){
     $(".mainWrap").slick({
         dots: true,
         infinite: true,
-        speed: 500,
-        // autoplay:true,
+        speed: 300,
+        autoplay:true,
         cssEase: "linear",
+        pauseOnHover: false,
+        pauseOnFocus: false,
     });
+
+    $(".arrivalSlide").slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        cssEase: "linear",
+        nextArrow:false,
+        prevArrow:false
+    });
+
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 5,
+        loop : true,
+        pagination: {
+          el: ".swiper-pagination",
+          type: "progressbar",
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
 });
 
